8cb3e4643dcc4b2ff7d1de273a4d9c40efe7a12a
mffvs1-creator <mffvs1@aluno.ifal.edu.br>
2026-09-01 12:01:16 -0300

    codeguin meio termando ( maio terined)

--- lib/api/adreesapi.dart
+++ lib/api/adreesapi.dart
@@ -1,12 +1,18 @@
+import 'package:rpgmaker/persona5/adress.dart';
 import 'package:dio/dio.dart';
 
-class api{
+class AddressApi {
  final dio = Dio();
- String baseurl = 'https://www.dnd5eapi.co/api';
+ String baseUrl = 'https://www.dnd5eapi.co/api';
 
- fyndByapi(String api) async {
-final response = await dio.get ('')
+ findByCep(String cep) async {
+  late Adress adress;
+  final response = await dio.get('$baseUrl/ws/$cep/json/');
 
+  if (response.statusCode == 200) {
+   Adress = adress.fromJson(response.data);
+  }
+
+  return Adress();
  }
-
 }
