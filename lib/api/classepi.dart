128c72d6396bf0eaf4a5ad7708b7388d7f438f7e
mffvs1-creator <mfvs1@aluno.ifal.edu.br>
2026-09-15 08:09:23 -0300

    little miodificação

--- lib/api/classepi.dart
+++ lib/api/classepi.dart
@@ -5,7 +5,7 @@ class classepi {
   final dio = Dio();
   String baseUrl = 'https://www.dnd5eapi.co/api/2014/classes';
 
-  findByCep(String classe) async {
+  findByClasse(String classe) async {
     late Persoapi persoapi;
     final response = await dio.get('$baseUrl/ws/$classe/json/');
 
